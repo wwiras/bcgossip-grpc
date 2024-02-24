5f99911bcfdf713d8914e582588dfdb696e8406e
wwiras <wwiras@gmail.com>
2024-02-24 09:39:40 +0800

    change from Service to Method in import dapr.ext.grpc

--- dapr/initiate.py
+++ dapr/initiate.py
@@ -6,11 +6,16 @@ from dapr.clients import DaprClient
 def initiate_gossip(target_service, message):
     with DaprClient() as d:
         print(f"Initiating gossip to {target_service}", flush=True)
-        d.invoke_service(
+        d.invoke_method(
             target_service,
             'my-method',
             data=json.dumps(message),
         )
+        # d.invoke_service(
+        #     target_service,
+        #     'my-method',
+        #     data=json.dumps(message),
+        # )
 
 
 if __name__ == "__main__":
--- dapr/node.py
+++ dapr/node.py
@@ -1,8 +1,8 @@
 import os
 import json
-from dapr.ext.grpc import App, InvokeServiceRequest, InvokeServiceResponse
+# from dapr.ext.grpc import App, InvokeServiceRequest, InvokeServiceResponse
 from dapr.clients import DaprClient
-
+from dapr.ext.grpc import InvokeMethodRequest, InvokeMethodResponse, App
 app = App()
 
 current_node = os.getenv('NODE_NAME')
@@ -10,7 +10,8 @@ neighbors = os.getenv('NEIGHBORS').split(',')
 
 
 @app.method(name='my-method')
-def my_method(request: InvokeServiceRequest) -> InvokeServiceResponse:
+# def my_method(request: InvokeServiceRequest) -> InvokeServiceResponse:
+def my_method(request: InvokeMethodRequest) -> InvokeMethodResponse:
     print(f"Received message at {current_node}: {request.text()}", flush=True)
     message = json.loads(request.text())
     sender = message['sender']
@@ -20,17 +21,23 @@ def my_method(request: InvokeServiceRequest) -> InvokeServiceResponse:
         if neighbor != sender:
             send_message(neighbor, message)
 
-    return InvokeServiceResponse(b'INVOKE_RECEIVED', "text/plain; charset=UTF-8")
+    # return InvokeServiceResponse(b'INVOKE_RECEIVED', "text/plain; charset=UTF-8")
+    return InvokeMethodResponse(b'INVOKE_RECEIVED', "text/plain; charset=UTF-8")
 
 
 def send_message(target_service, message):
     with DaprClient() as d:
         print(f"Sending message from {current_node} to {target_service}", flush=True)
-        d.invoke_service(
+        d.invoke_method(
             target_service,
             'my-method',
             data=json.dumps(message),
         )
+        # d.invoke_service(
+        #     target_service,
+        #     'my-method',
+        #     data=json.dumps(message),
+        # )
 
 
 if __name__ == "__main__":
