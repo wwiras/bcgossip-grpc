aa2d3cb73c59c086daad79460d3f57d7d86de566
wwiras <wwiras@gmail.com>
2024-04-15 23:59:52 +0800

    test script2

--- automation/test.py
+++ automation/test.py
@@ -1,4 +1,11 @@
+# sample using os comman
 import os
 os.system('pwd')
 os.system('cd ~')
 os.system('ls -la')
+
+# sample using system()
+stream = os.popen('ls -la')
+output = stream.readlines()
+print(output)
+
