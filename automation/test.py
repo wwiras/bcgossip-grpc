c348f6c9b21eeccd686519753ded02ff49fb158a
wwiras <wwiras@gmail.com>
2024-04-16 00:04:35 +0800

    test script3

--- automation/test.py
+++ automation/test.py
@@ -9,3 +9,12 @@ stream = os.popen('ls -la')
 output = stream.readlines()
 print(output)
 
+
+# STORING THE COMMAND OUTPUT TO A VARIABLE
+import subprocess
+x = subprocess.run(['ls', '-la'])
+print(x)
+print(x.args)
+print(x.returncode)
+print(x.stdout)
+print(x.stderr)
