82a4f8179159de95c0e62aae63b062c7168afc64
wwiras <wwiras@gmail.com>
2024-04-23 12:18:42 +0800

    test script41

--- automation/bcgossip1.py
+++ automation/bcgossip1.py
@@ -60,7 +60,7 @@ def delete_deployment(file_path, namespace='default', timeout=300):
                 return True
 
             print("Waiting for all pods to terminate...", flush=True)
-            time.sleep(2)  # Update status every 2 seconds
+            time.sleep(5)  # Update status every 5 seconds
 
         print("Timeout waiting for the resources to clear from the namespace.", flush=True)
         return False
