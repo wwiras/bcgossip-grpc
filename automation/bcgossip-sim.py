7ad3448b50c9795da6e6b8a5018c4062d8461980
wwiras <wwiras@gmail.com>
2024-04-25 10:44:36 +0800

    test bcgossip-sim 2

--- automation/bcgossip-sim.py
+++ automation/bcgossip-sim.py
@@ -81,15 +81,14 @@ def select_random_pod():
         raise Exception("No running pods found.")
     return random.choice(pod_list)
 
-def access_pod_and_initiate_gossip(pod_name, replicas, iteration):
+def access_pod_and_initiate_gossip(pod_name, replicas, unique_id):
     """
     Access the pod's shell, initiate gossip, and handle the response.
     """
     try:
         session = subprocess.Popen(['kubectl', 'exec', '-it', pod_name, '--', 'sh'], stdin=subprocess.PIPE,
                                    stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        unique_id = str(uuid.uuid4())[:5]  # Generate a unique ID
-        message = f'{unique_id}-cubaan{replicas}-{iteration}'
+        message = f'{unique_id}-cubaan{replicas}'
         session.stdin.write(f'python initiate.py --message {message}\n')
         session.stdin.flush()
         end_time = time.time() + 300
@@ -165,10 +164,11 @@ def main(num_tests, deployment_yaml):
 
     # Ensure pods are ready before proceeding
     if wait_for_pods_to_be_ready(namespace='default', expected_pods=replicas, timeout=300):
+        unique_id = str(uuid.uuid4())[:5]  # Generate a unique ID for the entire test
         for i in range(1, num_tests + 1):
             pod_name = select_random_pod()
             print(f"Selected pod: {pod_name}", flush=True)
-            if access_pod_and_initiate_gossip(pod_name, replicas, i):
+            if access_pod_and_initiate_gossip(pod_name, replicas, unique_id):
                 print(f"Test {i} complete.", flush=True)
             else:
                 print(f"Test {i} failed.", flush=True)
