59d42cc5d328daf489539f72efe6537a3360bc35
wwiras <wwiras@gmail.com>
2024-04-27 23:44:45 +0800

    test compare bandwidth 4

--- automation/bcgossip-bwidth.py
+++ automation/bcgossip-bwidth.py
@@ -159,7 +159,7 @@ def main(num_tests, deployment_folder):
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print("Usage: python bcgossip1.py <number_of_tests> <deployment_folder>")
+        print("Usage: python bcgossip-bwidth.py <number_of_tests> <deployment_folder>")
         sys.exit(1)
     num_tests = int(sys.argv[1])
     deployment_folder = sys.argv[2]
