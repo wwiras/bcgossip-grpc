384748f33079158025576ec7db9b8d26682df301
wwiras <wwiras@gmail.com>
2024-09-06 01:56:37 +0800

    k8sw9 v19

--- automation/bcgossip-w9.py
+++ automation/bcgossip-w9.py
@@ -240,6 +240,8 @@ def get_pod_names_and_ips(namespace="default"):
 
         pod_info = {}
         for line in output.splitlines():
+            print(line)
+            print(line.split())
             pod_name, pod_ip = line.split()
             pod_info[pod_name] = pod_ip
 
