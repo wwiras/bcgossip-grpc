bf1364391985a5f2d2c2ea04fd2e4825705fca7f
wwiras <wwiras@gmail.com>
2025-02-07 21:50:33 +0800

    remove return and print logs to quicker gossip 2

--- k8sw17/node.py
+++ k8sw17/node.py
@@ -115,8 +115,8 @@ class Node(gossip_pb2_grpc.GossipServiceServicer):
             self._log_event(message, sender_id, received_timestamp, None,
                             received_latency, 'initiate', log_message)
             self.gossip_initiated = False  # For multiple tests, need to reset gossip initialization
-            return gossip_pb2.Acknowledgment(details=f"{self.pod_name}({self.host}) processed message: '{message}'")
-
+            # return gossip_pb2.Acknowledgment(details=f"{self.pod_name}({self.host}) processed message: '{message}'")
+        #
         # Check for duplicate messages
         elif message in self.received_messages:
             log_message = (f"{self.pod_name}({self.host}) ignoring duplicate message: '{message}' "
@@ -134,7 +134,7 @@ class Node(gossip_pb2_grpc.GossipServiceServicer):
 
         # Gossip to neighbors (only if the message is new)
         self.gossip_message(message, sender_id)
-        # return gossip_pb2.Acknowledgment(details=f"{self.pod_name}({self.host}) processed message: '{message}'")
+        return gossip_pb2.Acknowledgment(details=f"{self.pod_name}({self.host}) processed message: '{message}'")
 
     # This function objective is to send message to all neighbor nodes.
     # In real environment, suppose we should get latency from
