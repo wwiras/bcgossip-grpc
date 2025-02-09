ff38a5a45b20bd5cc07aa31f4185eab82fc53192
wwiras <wwiras@gmail.com>
2025-02-09 10:58:27 +0800

    test to remove loggin basic conf from node.py

--- k8sw17/node.py
+++ k8sw17/node.py
@@ -11,7 +11,7 @@ import logging
 # import subprocess
 
 # Configure logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+# logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class Node(gossip_pb2_grpc.GossipServiceServicer):
