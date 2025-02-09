ca7f753884deb15708f5a15b3b48706d28b1ab49
wwiras <wwiras@gmail.com>
2025-02-09 19:57:43 +0800

    all files(topology) are to be tested

--- k8sw17/construct_kmeans.py
+++ k8sw17/construct_kmeans.py
@@ -445,7 +445,11 @@ if __name__ == '__main__':
     # filename = "nodes300_Feb062025152130_BA2.json"
     # filename = "nodes300_Feb072025183339_ER0.015.json"
     # filename = "nodes300_Feb092025140244_ER0.02.json"
-    filename = "nodes300_Feb092025140642_BA2.json"
+    # filename = "nodes300_Feb092025140642_BA2.json"
+    # filename = "nodes300_Feb092025184433_ER0.015.json"
+    # filename = "nodes300_Feb092025185345_ER0.015.json"
+    # filename = "nodes50_Feb092025192653_ER0.07.json"
+    filename = "nodes150_Feb092025194826_ER0.02.json"
 
 
 
