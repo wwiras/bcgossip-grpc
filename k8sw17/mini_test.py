e73f4a0efcb22a770c30588acc6b58d1ebfd23f7
wwiras <wwiras@gmail.com>
2025-02-19 15:29:58 +0800

    add mininet script v12

--- k8sw17/mini_test.py
+++ k8sw17/mini_test.py
@@ -5,5 +5,5 @@ tree4 = TreeTopo(depth=2,fanout=2)
 net = Mininet(topo=tree4)
 net.start()
 h1, h4  = net.hosts[0], net.hosts[3]
-print h1.cmd('ping -c1 %s' % h4.IP())
+print(f"h1.cmd('ping -c1 %s' % h4.IP()): {h1.cmd('ping -c1 %s' % h4.IP())}")
 net.stop()
