c55d05bf197a8e54df51f4f7ab4753e2ed94205a
wwiras <wwiras@gmail.com>
2025-02-19 17:07:39 +0800

    add mininet script v18

--- k8sw17/mininet/mini_test.py
+++ k8sw17/mininet/mini_test.py
@@ -10,23 +10,59 @@
 
 "clusterperf.py compare the maximum throughput between SSH and GRE tunnels"
 
-from mininet.examples.cluster import RemoteSSHLink, RemoteGRELink, RemoteHost
+# from mininet.examples.cluster import RemoteSSHLink, RemoteGRELink, RemoteHost
+# from mininet.net import Mininet
+# from mininet.log import setLogLevel
+#
+# def perf(Link):
+#     "Test connectivity nand performance over Link"
+#     net = Mininet( host=RemoteHost, link=Link, waitConnected=True )
+#     h1 = net.addHost( 'h1')
+#     h2 = net.addHost( 'h2', server='ubuntu2' )
+#     net.addLink( h1, h2 )
+#     net.start()
+#     net.pingAll()
+#     net.iperf()
+#     net.stop()
+#
+#
+# if __name__ == '__main__':
+#     setLogLevel('info')
+#     perf( RemoteSSHLink )
+#     perf( RemoteGRELink )
+
+# !/usr/bin/python
+
+from mininet.topo import Topo
 from mininet.net import Mininet
+from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
 
-def perf(Link):
-    "Test connectivity nand performance over Link"
-    net = Mininet( host=RemoteHost, link=Link, waitConnected=True )
-    h1 = net.addHost( 'h1')
-    h2 = net.addHost( 'h2', server='ubuntu2' )
-    net.addLink( h1, h2 )
+
+class SingleSwitchTopo(Topo):
+    "Single switch connected to n hosts."
+
+    def build(self, n=2):
+        switch = self.addSwitch('s1')
+        # Python's range(N) generates 0..N-1
+        for h in range(n):
+            host = self.addHost('h%s' % (h + 1))
+            self.addLink(host, switch)
+
+
+def simpleTest():
+    "Create and test a simple network"
+    topo = SingleSwitchTopo(n=4)
+    net = Mininet(topo)
     net.start()
+    print("Dumping host connections")
+    dumpNodeConnections(net.hosts)
+    print("Testing network connectivity")
     net.pingAll()
-    net.iperf()
     net.stop()
 
 
 if __name__ == '__main__':
+    # Tell mininet to print useful information
     setLogLevel('info')
-    perf( RemoteSSHLink )
-    perf( RemoteGRELink )
+    simpleTest()
