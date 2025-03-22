6912df871bc162eeb630d5c5496a165529114ff0
wwiras <wwiras@gmail.com>
2025-03-22 11:38:49 +0800

    automate with convergence

--- cnsim4/automate.py
+++ cnsim4/automate.py
@@ -69,43 +69,6 @@ class Test:
             traceback.print_exc()
             sys.exit(1)
 
-    # def run_command(self, command, full_path=None):
-    #     """
-    #     Runs a command and handles its output and errors.
-    #     """
-    #     try:
-    #         if isinstance(command, str):
-    #             result = subprocess.run(command, check=True, text=True, capture_output=True, shell=True)
-    #         else:
-    #             result = subprocess.run(command, check=True, text=True, capture_output=True)
-    #
-    #         if full_path:
-    #             if 'unchanged' in result.stdout or 'created' in result.stdout:
-    #                 print(f"{full_path} applied successfully!", flush=True)
-    #             elif 'deleted' in result.stdout:
-    #                 print(f"{full_path} deleted successfully!", flush=True)
-    #             else:
-    #                 print(f"Changes applied to {full_path}:", flush=True)
-    #                 print(result.stdout, flush=True)
-    #
-    #         print(f"result.stdout: {result.stdout}", flush=True)
-    #         return result.stdout, result.stderr
-    #     except subprocess.CalledProcessError as e:
-    #         if full_path:
-    #             print(f"An error occurred while applying {full_path}.", flush=True)
-    #         else:
-    #             print(f"An error occurred while executing the command.", flush=True)
-    #         print(f"Error message: {e.stderr}", flush=True)
-    #         traceback.print_exc()
-    #         sys.exit(1)
-    #     except Exception as e:
-    #         if full_path:
-    #             print(f"An unexpected error occurred while applying {full_path}.", flush=True)
-    #         else:
-    #             print(f"An unexpected error occurred while executing the command.", flush=True)
-    #         traceback.print_exc()
-    #         sys.exit(1)
-
     def wait_for_pods_to_be_ready(self, namespace='default', expected_pods=0, timeout=1000):
         """
         Waits for all pods in the specified namespace to be ready.
@@ -270,7 +233,8 @@ if __name__ == '__main__':
             unique_id = str(uuid.uuid4())[:4]  # Generate a UUID and take the first 4 characters
 
             # Test iteration starts here
-            for nt in range(1, test.num_tests + 1):
+            # iteration=0 for convergence
+            for nt in range(0, test.num_tests + 1):
                 pod_name = test.select_random_pod()
                 print(f"Selected pod: {pod_name}", flush=True)
                 if test.access_pod_and_initiate_gossip(pod_name, test.num_nodes, unique_id, nt):
